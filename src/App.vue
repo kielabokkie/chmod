9c0f30632f3d66dbdabdecb5fbf35b68ee833b83
kielabokkie <kielabokkie@gmail.com>
2016-07-03 21:10:42 +1200

    Initial commit 🔑

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <chmod></chmod>
+  </div>
+</template>
+
+<script>
+import Chmod from './components/Chmod'
+
+export default {
+  components: {
+    Chmod
+  }
+}
+</script>
+
+<style>
+html {
+  height: 100%;
+  background: #0299A5;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  color: #fff;
+  margin-top: -100px;
+  max-width: 600px;
+  font-family: 'Ubuntu', Source Sans Pro, Helvetica, sans-serif;
+  text-align: center;
+}
+</style>
---
+++ src/components/Chmod.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <h1>chmod</h1>
+  </div>
+  <input placeholder="rwxr-xr-x" size="9" v-model="permissions" class="large" v-on:input="parse">
+  <div class="result">{{ result }}</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      permissions: '',
+      result: '000'
+    }
+  },
+  methods: {
+    parse: function () {
+      if (this.permissions.length !== 9) {
+        return
+      }
+      var sectionOne = this.parseSection(1)
+      var sectionTwo = this.parseSection(2)
+      var sectionThree = this.parseSection(3)
+      this.result = sectionOne.toString() + sectionTwo.toString() + sectionThree.toString()
+    },
+    parseSection: function (section) {
+      var start = 0
+      switch (section) {
+        case 2:
+          start = 3
+          break
+        case 3:
+          start = 6
+          break
+      }
+      var sectionString = this.permissions.substring(start, start + 3)
+      var first = this.getPermission(sectionString.substring(0, 1))
+      var second = this.getPermission(sectionString.substring(1, 2))
+      var third = this.getPermission(sectionString.substring(2, 3))
+      return first + second + third
+    },
+    getPermission: function (character) {
+      switch (character) {
+        case 'r':
+          return 4
+        case 'w':
+          return 2
+        case 'x':
+          return 1
+        default:
+          return 0
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  font-size: 60px;
+  margin-bottom: 20px;
+}
+input {
+  padding: 5px 10px 5px 5px;
+  margin-bottom: 10px;
+  display: block;
+  border: none;
+  text-align: center;
+}
+input:focus {
+  outline:none;
+}
+.result {
+  font-size: 100px;
+  font-weight: bold;
+}
+.large {
+  font-size: 50px;
+  font-weight: bold;
+}
+</style>
